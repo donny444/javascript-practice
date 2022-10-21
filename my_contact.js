4ba34e2fc3780e0cfa3fefc3ca4149a4b2389ebe
donny_444 <104888835+donny444@users.noreply.github.com>
2022-10-21 14:20:02 +0700

    Add files via upload

---
+++ my_contact.js
@@ -0,0 +1,14 @@
+var myContact = {
+    "facebook": "Tanadorn Rachatapeeti",
+    "instagram": "donny_galaxy",
+    "tiktok": "dekchaipluto",
+    "youtubechannel": "Tanadorn GT",
+    "facebookpage": "ดอนนวย",
+}
+switch("Facebook Page") {
+    case "Facebook": console.log(myContact.facebook); break;
+    case "Instagram": console.log(myContact.instagram); break;
+    case "TikTok": console.log(myContact.tiktok); break;
+    case "Youtube Channel": console.log(myContact.youtubechannel); break;
+    case "Facebook Page": console.log(myContact.facebookpage); break;
+}
---
+++ my_gaming_gears.js
@@ -0,0 +1,39 @@
+var myMice = {
+    "brand": "Razer",
+    "color": "Black",
+    "weight": undefined,
+    "connection": "USB-A",
+    "rgb": false
+}
+var myKeyboard = {
+    "brand": "Logitech",
+    "color": "Black",
+    "weight": undefined,
+    "connection": "USB-A",
+    "rgb": true,
+}
+var myHeadset = {
+    "brand": "Razer",
+    "color": "Black",
+    "weight": undefined,
+    "connection": "USB-A",
+    "rgb": true
+}
+var myMousepad = {
+    "brand": "Zowie",
+    "color": "Black",
+    "weight": undefined,
+    "connection": null,
+    "rgb": false
+}
+var myHeadsetHolder = {
+    "brand": "Corsair",
+    "color": "Black",
+    "weight": undefined,
+    "connection": "USB-A",
+    "rgb": true
+}
+//Gaming Gear Object
+
+console.log(myMousepad.connection)
+//Check
