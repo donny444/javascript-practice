871b545823982e26fdbd809e911a6549e0d8393b
donny_444 <104888835+donny444@users.noreply.github.com>
2023-07-29 23:31:23 +0700

    unfinished

---
+++ data_structures.js
@@ -0,0 +1,8 @@
+import LinkedList from "./linked_list.js";
+import Stack from "./stack.js";
+import Queue from "./queue.js";
+
+const data1 = LinkedList("มด2");
+data1.push("มด3");
+data1.unshift("มด1");
+data1.showlist()
